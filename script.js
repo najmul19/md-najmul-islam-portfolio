31597cf9d990738db999137a4c14ab4b37afb734
najmul <mdnajmulislam10992@gmail.com>
2025-06-04 21:46:57 +0600

    completed the typed animation

---
+++ script.js
@@ -0,0 +1,14 @@
+var type = new Typed("#typed", {
+  strings: [
+    "Mern-Stack Web Developer",
+    "React Native Developer",
+    "Flutter Developer",
+    "PHP Developer",
+    "Machine Learning Engineer",
+  ],
+  tupeSpeed: 50,
+  backSpeed: 20,
+  backDelay: 3000,
+  showCursor: false,
+  loop: true,
+});

0a0a69236684db75af4c8362ff075ffe2ce0f4a5
najmul <mdnajmulislam10992@gmail.com>
2025-06-04 22:01:30 +0600

    completed the dark and light theme toggle

--- script.js
+++ script.js
@@ -12,3 +12,22 @@ var type = new Typed("#typed", {
   showCursor: false,
   loop: true,
 });
+
+
+// theme
+
+function theme(){
+    const darkBtn = document.getElementById("darkBtn");
+    const lightBtn = document.getElementById("lightBtn");
+
+    document.body.classList.toggle('light');
+
+    if(document.body.classList.contains("light")){
+        
+        darkBtn.style.display="none";
+        lightBtn.style.display="block";
+    } else {
+        darkBtn.style.display="block";
+        lightBtn.style.display="none";
+    }
+}
